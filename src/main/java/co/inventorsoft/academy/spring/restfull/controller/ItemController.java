0b4f8c58e1d20a2e9f4ffabcb888a3932f108128
appsterlight <appsterlight@gmail.com>
2023-10-18 18:46:41 +0300

    Clean up unused imports

--- src/main/java/co/inventorsoft/academy/spring/restfull/controller/ItemController.java
+++ src/main/java/co/inventorsoft/academy/spring/restfull/controller/ItemController.java
@@ -8,18 +8,13 @@ import co.inventorsoft.academy.spring.restfull.util.MapperUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/co/inventorsoft/academy/spring/restfull/dao/CustomerRepository.java
+++ src/main/java/co/inventorsoft/academy/spring/restfull/dao/CustomerRepository.java
@@ -1,11 +1,8 @@
 package co.inventorsoft.academy.spring.restfull.dao;
 
 import co.inventorsoft.academy.spring.restfull.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
