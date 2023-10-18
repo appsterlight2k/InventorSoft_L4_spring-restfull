75b272fc9ccefa66e48a35cdd9f22afbe7078372
appsterlight <appsterlight@gmail.com>
2023-10-18 18:36:08 +0300

    Add DTO classes: ItemDto, CustomerDto, OrderDto

---
+++ src/main/java/co/inventorsoft/academy/spring/restfull/dto/CustomerDto.java
@@ -0,0 +1,16 @@
+package co.inventorsoft.academy.spring.restfull.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerDto {
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+
+}
