a9167b78d058dfedeac51557decf8ad4d07c2d17
appsterlight <appsterlight@gmail.com>
2023-10-18 18:44:44 +0300

    Add WebResponse class for standardized DTO responses

---
+++ src/main/java/co/inventorsoft/academy/spring/restfull/dto/WebResponse.java
@@ -0,0 +1,14 @@
+package co.inventorsoft.academy.spring.restfull.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class WebResponse<T> {
+    private T data;
+    private String message;
+    private boolean success;
+}
