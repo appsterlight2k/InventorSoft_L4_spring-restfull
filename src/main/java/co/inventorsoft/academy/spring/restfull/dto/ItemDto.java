023d3f7f8d53640ef301324a6063b664c092a8b3
appsterlight <appsterlight@gmail.com>
2023-10-19 15:07:55 +0300

    Add validation annotations to  ItemDto

--- src/main/java/co/inventorsoft/academy/spring/restfull/dto/ItemDto.java
+++ src/main/java/co/inventorsoft/academy/spring/restfull/dto/ItemDto.java
@@ -1,5 +1,9 @@
 package co.inventorsoft.academy.spring.restfull.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,10 +11,22 @@ import lombok.Setter;
 @Setter
 public class ItemDto {
     private Long id;
+
+    @NotBlank
+    @Size(min = 5, max = 2000)
     private String name;
+
+    @NotNull
+    @Min(0)
     private Integer count;
+
+    @NotNull
+    @Min(0)
     private Double price;
+
+    @NotNull
     private String article_num;
+
     private String description;
 
 }
