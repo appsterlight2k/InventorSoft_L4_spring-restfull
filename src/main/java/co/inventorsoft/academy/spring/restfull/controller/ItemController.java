4184a52cd5f171dce468fa908022d8b250d22c6b
appsterlight <appsterlight@gmail.com>
2023-10-19 15:52:40 +0300

    - Refactor constructor-based injection for ItemRepository; - Standardize all endpoints with "/items" prefix; - Simplify mapping annotations by removing unnecessary parameters; - Implement getWebResponseEntity() for individual value WebResponses;

--- src/main/java/co/inventorsoft/academy/spring/restfull/controller/ItemController.java
+++ src/main/java/co/inventorsoft/academy/spring/restfull/controller/ItemController.java
@@ -2,12 +2,13 @@ package co.inventorsoft.academy.spring.restfull.controller;
 
 import co.inventorsoft.academy.spring.restfull.dto.ItemDto;
 import co.inventorsoft.academy.spring.restfull.dto.WebResponse;
+import co.inventorsoft.academy.spring.restfull.exception.ItemNotFoundException;
 import co.inventorsoft.academy.spring.restfull.model.Item;
 import co.inventorsoft.academy.spring.restfull.service.ItemService;
 import co.inventorsoft.academy.spring.restfull.util.MapperUtil;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,85 +19,78 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class ItemController {
-    @Autowired
-    private ItemService itemService;
-
+    private final ItemService itemService;
     @Autowired
     private MapperUtil mapperUtil;
 
-
-    @PostMapping("/items/new")
-    public ItemDto create(@RequestBody ItemDto itemDto) {
-        Item item = mapperUtil.convertToEntity(itemDto, Item.class);
-        Item updated = itemService.save(item);
-
-        return mapperUtil.convertToDto(updated, ItemDto.class);
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
     }
 
-    @GetMapping(value = "/items")
-    public ResponseEntity<WebResponse<List<ItemDto>>> getAllItems() {
+    @PostMapping("/items")
+    public WebResponse<ItemDto> create(@Valid @RequestBody ItemDto itemDto) {
+        Item item = mapperUtil.convertToEntity(itemDto, Item.class);
+        Item created = itemService.save(item);
+
+        return getWebResponseEntity(created, "Item was created successfully");
+    }
+
+    @GetMapping("/items")
+    public WebResponse<List<ItemDto>> getAllItems() {
         List<Item> items = itemService.findAll();
 
-        return getWebResponseEntityForAll(items);
+        return getWebResponseEntityForAll(items, "Items were fetched successfully");
     }
 
-    //items?price=100
-    @GetMapping(value = "/items",  params = "price")
-    public ResponseEntity<WebResponse<List<ItemDto>>> getItemsByPrice(@RequestParam Double price) {
+    //example: /items?price=100
+    @GetMapping(value = "/items", params = "price")
+    public WebResponse<List<ItemDto>> getItemsByPrice(@RequestParam Double price) {
         List<Item> items = itemService.findAllByPrice(price);
 
-        return getWebResponseEntityForAll(items);
+        return getWebResponseEntityForAll(items, "Items were fetched by price successfully");
+    }
+    @GetMapping("/items/{id}")
+    public WebResponse<ItemDto> getItemById(@PathVariable Long id) {
+        return itemService.getById(id)
+                .map(item -> getWebResponseEntity(item, "Item was fetched successfully"))
+                .orElseThrow(() -> new ItemNotFoundException("Item was not found"));
     }
 
-    @GetMapping(value = "/items/{id}")
-    public ResponseEntity<WebResponse<ItemDto>> getItemById(@PathVariable Long id) {
-        Optional<ItemDto> optionalDto = itemService.getById(id).map(item -> mapperUtil.convertToDto(item, ItemDto.class));
-
-        boolean isSuccess = optionalDto.isPresent();
-        String message = isSuccess ? "Item was fetched successfully" : "Item was not found!";
-        Integer count = isSuccess ? 1 : 0;
-        WebResponse<ItemDto> response = new WebResponse<>(optionalDto.orElse(null), message, isSuccess, count);
-        HttpStatus status = isSuccess ? HttpStatus.OK : HttpStatus.NOT_FOUND;
-
-        return new ResponseEntity<>(response, status);
-    }
-
-    //items?name=Jedi
+    //example: /items?name=Jedi
     @GetMapping(value = "/items", params = "name")
-    public ResponseEntity<WebResponse<List<ItemDto>>> findItemsByNameContains(@RequestParam("name") String text) {
+    public WebResponse<List<ItemDto>> findItemsByNameContains(@RequestParam("name") String text) {
         List<Item> items = itemService.findAllByNameContains(text);
 
-        return getWebResponseEntityForAll(items);
+        return getWebResponseEntityForAll(items, "Items were fetched by name successfully");
     }
 
     @PutMapping("/items/{id}")
-    public ItemDto update(@PathVariable Long id, @RequestBody ItemDto itemDto) {
+    public WebResponse<ItemDto> update(@PathVariable Long id, @Valid @RequestBody ItemDto itemDto) {
         Item item = mapperUtil.convertToEntity(itemDto, Item.class);
         item.setId(id);
         Item updated = itemService.save(item);
 
-        return mapperUtil.convertToDto(updated, ItemDto.class);
+        return getWebResponseEntity(item, "Item was updated successfully");
     }
 
-    @DeleteMapping(value = "/delete/{id}")
+    @DeleteMapping("/items/{id}")
     public void deleteById(@PathVariable Long id) {
         itemService.deleteById(id);
     }
 
-    private ResponseEntity<WebResponse<List<ItemDto>>> getWebResponseEntityForAll(List<Item> items) {
+    private WebResponse<ItemDto> getWebResponseEntity(Item item, String successMessage) {
+        ItemDto itemDto = mapperUtil.convertToDto(item, ItemDto.class);
+
+        return new WebResponse<>(itemDto, successMessage, true, 1);
+    }
+    private WebResponse<List<ItemDto>> getWebResponseEntityForAll(List<Item> items, String successMessage) {
         List<ItemDto> itemsDto = mapperUtil.convertToDtoList(items, ItemDto.class);
 
-        boolean isSuccess = !itemsDto.isEmpty();
-        String message = isSuccess ? "Items were fetched successfully" : "Items were not found!";
-
-        WebResponse<List<ItemDto>> response = new WebResponse<>(isSuccess ? itemsDto : null, message, isSuccess, itemsDto.size());
-        HttpStatus status = isSuccess ? HttpStatus.OK : HttpStatus.NOT_FOUND;
-
-        return new ResponseEntity<>(response, status);
+        return new WebResponse<>(itemsDto, successMessage, true, itemsDto.size());
     }
 
 }
